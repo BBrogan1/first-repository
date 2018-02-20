291fe857bb03d65d373924a2ee643f4ca4568e54
BEN BROGAN - STUDENT <G00357311@gmit.ie>
2018-02-20 13:50:16 +0000

    week 20 lab ex 3

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Addition{
+     public static void main(String[] args){
+	  Scanner input = new Scanner(System.in);
+	  int total = 0;
+	  System.out.print("Enter numbers (ctrl-z to end):");
+	  System.out.println();
+
+	  while(input.hasNext()){
+		  total += input.nextInt();
+	  }
+	  System.out.println("total is:"+total);
+  }
+}
