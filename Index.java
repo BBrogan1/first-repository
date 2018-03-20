67be07648e5a76cc560d76c4097b0712adb92bd8
BEN BROGAN - STUDENT <G00357311@gmit.ie>
2018-03-20 13:56:54 +0000

    week 24

--- Index.java
+++ Index.java
@@ -16,7 +16,8 @@ public class Index{
     System.out.println("7. Reverse");
     System.out.println("8. FirstArray");
     System.out.println("9. SecondArray");
-    System.out.println("10. Exit");
+    System.out.println("10. SearchStudent");
+    System.out.println("11. Exit");
 
     System.out.println();
 
@@ -70,6 +71,11 @@ public class Index{
 		SecondArray.main(null);
         break;
       case 10:
+		System.out.println("SearchStudent");
+		System.out.println();
+		SearchStudent.main(null);
+        break;
+      case 11:
         System.out.println("Exiting....");
         break;
       default:
